b2d713a4fa2602fd6c33b1ae1870815253e04340
Kim <ellkim@deloitte.com>
2021-11-09 11:49:14 -0500

    env vars

--- src/test/java/com/example/springboot/HelloControllerIT.java
+++ src/test/java/com/example/springboot/HelloControllerIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,10 +15,11 @@ public class HelloControllerIT {
 
 	@Autowired
 	private TestRestTemplate template;
-
+    @Autowired
+    private Environment env;
     @Test
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/", String.class);
-        assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot! Docker  demoservice");
+        assertThat(response.getBody()).isEqualTo("Greetings from Spring Boot! Docker  " + env.getProperty("spring.application.name"));
     }
 }
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,11 +20,12 @@ public class HelloControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
-
+	@Autowired
+	private Environment env;
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Spring Boot! Docker  demoservice")));
+				.andExpect(content().string(equalTo("Greetings from Spring Boot! Docker  " + env.getProperty("spring.application.name"))));
 	}
 }
